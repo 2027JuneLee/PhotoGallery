ebf8dd36cd1477497ad9f48e47a78cab5e2086fc
ellie <ellie@kmong.com>
2022-08-13 12:52:28 +0900

    comments page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import './App.css';
-import LoginPage from './LoginPage';
-import RealLoginPage from './RealLoginPage';
-import SignUpPage from './SignUpPage';
-import SelectWally from './SelectionPage'
-import {BrowserRouter as  Router, Routes, Route } from 'react-router-dom';
+import "./App.css";
+import LoginPage from "./LoginPage";
+import RealLoginPage from "./RealLoginPage";
+import SignUpPage from "./SignUpPage";
+import SelectWally from "./SelectionPage/index";
+import SelectRacoon from "./SelectionPage/index2";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-  
-      <Router>
-        <div>
-          <Routes>
-        <Route path="/" element={<LoginPage/>}/>
-        <Route path="/signup" element={<SignUpPage/>}/>
-        <Route path="/login" element={<RealLoginPage/>}/>
-        <Route path='/select-1' element={<SelectWally/>}/>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/login" element={<RealLoginPage />} />
+          <Route path="/select-1" element={<SelectWally />} />
+          <Route path="/select-2" element={<SelectRacoon />} />
         </Routes>
-        </div>
-      </Router>
-      
-
+      </div>
+    </Router>
   );
 }
 
---
+++ src/SelectionPage/index2.js
@@ -0,0 +1,103 @@
+import styled from "styled-components";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Img1 from "./raccoon-1.jpg";
+import Img2 from "./raccoon-2.png";
+
+import Img4 from "./raccoon-4.png";
+import Img5 from "./raccoon-5.png";
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  flex-direction: column;
+  color: black;
+  justify-content: center;
+  align-items: center;
+  // text-align: center;
+  font-family: Arial Narrow, sans-serif;
+  background-image: url({WALLYWORLD});
+  opacity: 0.5;
+  font-family: "Dancing Script", cursive;
+  opacity: 0.5;
+  background: repeating-linear-gradient(
+    45deg,
+    #ff0000,
+    #ff0000 50px,
+    #f5fffa 10px,
+    #f5fffa 20px
+  ); ;
+`;
+
+const Input = styled.input`
+  border-radius: 5px;
+  background: black;
+  font-size: 14px;
+  border: none;
+  color: white;
+  width: 500px;
+  height: 25px;
+  padding: 8px;
+`;
+const SliderWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
+const SliderButton = styled.button`
+  font-size: 40px;
+  color: black;
+  font-weight: 700;
+  border: none;
+`;
+
+const Block = styled.img`
+  background-color: yellow;
+  width: 250px;
+  height: 250px;
+  margin-right: 20px;
+  margin-left: 20px;
+`;
+function MainPage() {
+  const navigate = useNavigate();
+  const [RacoonIndex, setRacoonIndex] = useState({
+    first: 0,
+    last: 4,
+  });
+  const images = [Img1, Img2, Img4, Img5];
+
+  const nextPage = (action) => {
+    if (action == "prev" && RacoonIndex.first == 4) {
+      setRacoonIndex({
+        first: 0,
+        last: 4,
+      });
+    }
+    if (action == "next" && RacoonIndex.first == 0) {
+      setRacoonIndex({
+        first: 4,
+        last: 8,
+      });
+    }
+  };
+
+  return (
+    <Wrapper>
+      <h2>SELECT YOUR CHARACTER: </h2>
+      <br></br>
+      <SliderWrapper>
+        <SliderButton onClick={() => nextPage("prev")}>
+          <span>&#8592;</span>
+        </SliderButton>
+        {images.slice(RacoonIndex.first, RacoonIndex.last).map((image) => (
+          <Block src={image} />
+        ))}
+
+        <SliderButton onClick={() => nextPage("next")}>
+          <span>&#8594;</span>
+        </SliderButton>
+      </SliderWrapper>
+    </Wrapper>
+  );
+}
+export default MainPage;
